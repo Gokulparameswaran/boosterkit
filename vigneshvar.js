0ec4a6c41f268a5690b2b6678b4712e910ef264d
Gokul P <balamanibalamani452@gmail.com>
2025-06-02 06:12:24 +0530

    first commit

---
+++ vigneshvar.js
@@ -0,0 +1,19 @@
+let letter =prompt("Enter the letter :");
+switch(letter){
+   case 'a':
+   case 'i':
+   case 'u':
+   case 'o':
+   case 'e':
+   case 'A':
+   case 'I':
+   case 'U':
+   case 'O':
+   case 'E':
+      console.log(letter+" is a vowel");
+      break;
+   default:
+      console.log(letter+" is not vowel ");
+    
+}
+
